3e56cafbb8dced79cfb42dbbd091a1d00d38fdbd
crookedseahawk <nixonharlenbean@gmail.com>
2025-12-04 18:14:10 -0700

    hee hee hee haw

--- backend/data/scrape.py
+++ backend/data/scrape.py
@@ -1,14 +1,16 @@
 import requests as re
 from bs4 import BeautifulSoup as bs
 
-raw = "/raw_data/gutenberg_bible.html"
+raw = "raw_data/pg10-images.html"
 
 def obtain_soup(url):
-  request = re.get(url)
-  soup = bs(request.text,features="html.parser")
-  return soup
+  with open(url,"r",encoding="utf-8",errors="ignore") as file:
+    bible = file.read()
+  soup = bs(bible,features="html.parser")
+  p_tag = soup.find("p")
+  return p_tag
 
 bible_soup = obtain_soup(raw)
 with open("prep_data.json", "w") as file:
-  file.write(bible_soup[0])
+  file.write(bible_soup.text)
   
