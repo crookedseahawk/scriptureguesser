247088a5e8444453f62c463a590ff7f1206bf8f7
crookedseahawk <nixonharlenbean@gmail.com>
2025-12-08 17:33:31 -0700

    final_scores

--- backend/game/scoring.py
+++ backend/game/scoring.py
@@ -1,20 +1,18 @@
 from backend.data.dictionaries import *
+from math import exp
 
-def score(guess,correct):
+def calc_score(guess,correct):
     MAX_POINTS = 5000
-    weights = [0,.2,.3,.5]
-    book_distance = 3
-    if guess == correct:
-        return MAX_POINTS
-    if guess[0] != correct[0]:
-        return 0
-    for i in range():
-        pass
-    if guess[1] != correct[1]:
-        return max(MAX_POINTS * (1 - (abs(guess[1]-correct[1]))/book_distance), 0)
-    if guess[2] != correct[2]:
-        return max(MAX_POINTS * (1 - (abs(guess[2]-correct[2]))/book_distance), 0)
-    if guess[3] != correct[3]:
-        return max(MAX_POINTS * (1 - (abs(guess[3]-correct[3]))/book_distance), 0)
-    
-print(score([0,0,0,0],[0,0,1,0]))
+    D = 200
+    weights = [400,100,20,1]
+    total = 0
+    for i in range(4):
+        if guess[i] == correct[i][1]:
+            total += 0
+        else:
+            total += weights[i] * abs(guess[i]-correct[i][1])
+
+    return int(MAX_POINTS * exp(-total/D))
+
+
+print(calc_score([0,0,0,0],[(0,1),(0,0),(0,0),(0,0)]))
